9c2da7c133625313eddf63d23a9c09498630101b
Dennis Amparo <dennisamparo11@gmail.com>
2021-08-09 18:42:12 -0700

    building fuctionality

---
+++ script.js
@@ -0,0 +1,6 @@
+const playerName = document.getElementById("#playerName");
+const ballControl = document.getElementById("#inputBallcontrol");
+const passing = document.getElementById("#inputPassing");
+const shooting = document.getElementById("#inputShooting");
+const stamina = document.getElementById("#inputStamina");
+const fieldAwareness = document.getElementById("#inputFieldawareness");
