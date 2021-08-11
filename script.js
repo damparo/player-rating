f6d3b810ad3e76e5cbd71b6458518d1e2894183e
Dennis Amparo <dennisamparo11@gmail.com>
2021-08-10 23:08:23 -0700

    player object created

--- script.js
+++ script.js
@@ -1,6 +1,46 @@
-const playerName = document.getElementById("#playerName");
-const ballControl = document.getElementById("#inputBallcontrol");
-const passing = document.getElementById("#inputPassing");
-const shooting = document.getElementById("#inputShooting");
-const stamina = document.getElementById("#inputStamina");
-const fieldAwareness = document.getElementById("#inputFieldawareness");
+const playerName = document.getElementById("playerName");
+const ballControl = document.getElementById("inputBallcontrol");
+const passing = document.getElementById("inputPassing");
+const shooting = document.getElementById("inputShooting");
+const stamina = document.getElementById("inputStamina");
+const fieldAwareness =document.getElementById("inputFieldawareness");
+const saveBtn = document.getElementById("saveBtn");
+
+saveBtn.addEventListener("click", function(event){
+
+    event.preventDefault();
+    const playerData = {
+
+
+    playerNom: playerName.value.trim(),
+    ballCon: ballControl.value.trim(),
+    pass: passing.value.trim(),
+    shoot: shooting.value.trim(),
+    stam: stamina.value.trim(),
+    fieldAware: fieldAwareness.value.trim(),
+
+};
+
+console.log(playerData);
+
+
+})
+// saveBtn.addEventListener("click", function(event){
+
+// console.log("hello");
+
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
