29bda41258654e6a70fd15b8105874cb9756a797
Dennis Amparo <dennisamparo11@gmail.com>
2021-08-21 19:03:40 -0700

    displays error message but can alse be removed after input is added

--- script.js
+++ script.js
@@ -6,36 +6,36 @@ const stamina = document.getElementById("inputStamina");
 const fieldAwareness = document.getElementById("inputFieldawareness");
 const saveBtn = document.getElementById("saveBtn");
 const playerScore = document.getElementById("score");
-const message1 = document.getElementById("message")
+const message1 = document.getElementById("message");
 
 // create function if input is left blank
 
+function displayError(message) {
 
-function displayError (message) {
-
-  message1.textContent = message;
-
-
+  switch (message){
+    case "Please fill out all fields":
+      message1.textContent = message;
+      break;
+    case "everthingelse":
+      message1.textContent = "";
+      break;
+  }
 }
-
-
-
-
-
 
 saveBtn.addEventListener("click", function (event) {
   event.preventDefault();
 
-
-
-
-
-if (playerName.value === "" || ballControl.value === "" || passing.value === "" || shooting.value  === "" || stamina.value === "" || fieldAwareness.value === ""){
-  displayError("Please fill out all fields");
-};
-
-
-
+  if (
+    playerName.value === "" ||
+    ballControl.value === "" ||
+    passing.value === "" ||
+    shooting.value === "" ||
+    stamina.value === "" ||
+    fieldAwareness.value === ""
+  ) {
+    displayError("Please fill out all fields");
+  }else { 
+  displayError("everthingelse")
   const playerData = {
     playerNom: playerName.value.trim(),
     ballCon: ballControl.value,
@@ -47,36 +47,21 @@ if (playerName.value === "" || ballControl.value === "" || passing.value === "" 
 
   console.log(playerData);
 
-
-
   addScores();
-  function addScores (){
-
-
-    const overallScore = parseInt(ballControl.value) + parseInt(passing.value) + parseInt(shooting.value) + parseInt(stamina.value) + parseInt(fieldAwareness.value)
-    //  + passing + shooting + stamina + fieldAwareness;
+  function addScores() {
+    const overallScore =
+      parseInt(ballControl.value) +
+      parseInt(passing.value) +
+      parseInt(shooting.value) +
+      parseInt(stamina.value) +
+      parseInt(fieldAwareness.value);
+    
 
     console.log(overallScore);
 
-
     playerScore.innerHTML = overallScore;
-
-
-
-
-
+  }
   }
  
-
-
-
-//   console.log(addScores());
-
-
-
 });
-// saveBtn.addEventListener("click", function(event){
 
-// console.log("hello");
-
-// });
