4d56444e71e83b6f1f716fa8a73135f25fabb9cd
Dennis Amparo <dennisamparo11@gmail.com>
2021-08-21 17:10:50 -0700

    can take sum of inputs

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const playerName = document.getElementById("playerName");
 const ballControl = document.getElementById("inputBallcontrol");
-// const passing = document.getElementById("inputPassing");
-// const shooting = document.getElementById("inputShooting");
-// const stamina = document.getElementById("inputStamina");
-// const fieldAwareness = document.getElementById("inputFieldawareness");
+const passing = document.getElementById("inputPassing");
+const shooting = document.getElementById("inputShooting");
+const stamina = document.getElementById("inputStamina");
+const fieldAwareness = document.getElementById("inputFieldawareness");
 const saveBtn = document.getElementById("saveBtn");
 
 saveBtn.addEventListener("click", function (event) {
@@ -15,10 +15,10 @@ saveBtn.addEventListener("click", function (event) {
   const playerData = {
     playerNom: playerName.value.trim(),
     ballCon: ballControl.value,
-    // pass: passing.value.trim(),
-    // shoot: shooting.value.trim(),
-    // stam: stamina.value.trim(),
-    // fieldAware: fieldAwareness.value.trim(),
+    pass: passing.value,
+    shoot: shooting.value,
+    stam: stamina.value,
+    fieldAware: fieldAwareness.value,
   };
 
   console.log(playerData);
@@ -29,7 +29,7 @@ saveBtn.addEventListener("click", function (event) {
   function addScores (){
 
 
-    const overallScore = parseInt(ballControl.value) + parseInt(ballControl.value);
+    const overallScore = parseInt(ballControl.value) + parseInt(passing.value) + parseInt(shooting.value) + parseInt(stamina.value) + parseInt(fieldAwareness.value)
     //  + passing + shooting + stamina + fieldAwareness;
 
     console.log(overallScore);
